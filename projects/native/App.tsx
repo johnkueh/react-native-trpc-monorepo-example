9000720aaf7415265c51fda899daa0218797f56d
John Kueh <john.kueh@gmail.com>
2022-12-29 15:20:12 +1100

    feat: make trpc calls from expo app

--- projects/native/App.tsx
+++ projects/native/App.tsx
@@ -4,9 +4,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthedStack from './features/auth/AuthedStack';
 import UnauthedStack from './features/auth/UnauthedStack';
 import { useAuthentication } from './features/auth/useAuth';
-import useCachedResources from './hooks/useCachedResources';
+import useCachedResources from './utils/useCachedResources';
 import { theme } from './features/design-system/theme';
 import { ThemeProvider } from './features/design-system/styled-components';
+import { TRPCProvider } from './utils/TRPCProvider';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -21,9 +22,11 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <StatusBar style="dark" />
-        <ThemeProvider theme={theme}>
-          {isLoadingAuth ? <View /> : isAuthed ? <AuthedStack /> : <UnauthedStack />}
-        </ThemeProvider>
+        <TRPCProvider>
+          <ThemeProvider theme={theme}>
+            {isLoadingAuth ? <View /> : isAuthed ? <AuthedStack /> : <UnauthedStack />}
+          </ThemeProvider>
+        </TRPCProvider>
       </SafeAreaProvider>
     );
   }
