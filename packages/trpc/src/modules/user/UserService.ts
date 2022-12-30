f98e67fb252196bde0cf85d047fcac38932fd3af
John Kueh <john.kueh@gmail.com>
2022-12-30 12:34:00 +1100

    feat: retrieve user from database

--- packages/trpc/src/modules/user/UserService.ts
+++ packages/trpc/src/modules/user/UserService.ts
@@ -6,10 +6,10 @@ export class UserService {
   constructor(private db: Database) {}
 
   public getUserById = async (id: string) => {
-    return {
-      id: id,
-      name: 'John Doe',
-      email: 'john.doe@gmail.com',
-    };
+    return this.db.client.user.findFirst({
+      where: {
+        id: id,
+      },
+    });
   };
 }
